ed4ecab4b476647895eb0fada63c1e7cf7452bb6
luyen3112 <89532499+luyen3112@users.noreply.github.com>
2023-06-09 10:34:38 -0700

    Update f88_interface.py

--- f88_interface.py
+++ f88_interface.py
@@ -40,7 +40,7 @@ sc = joblib.load('w/std_scaler.save')
 @st.cache_data
 def get_data():
     df = pd.read_parquet("final_preprocess.parquet",engine = 'fastparquet')
-    fp_model = "20230603_003037.pkl"
+    fp_model = pickle.load(open("20230603_003037.pkl", 'rb'))
     return df, fp_model
 
 df,  fp_model= get_data()
